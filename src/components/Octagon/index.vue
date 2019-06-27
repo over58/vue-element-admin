e3c2f1ad12c8cf8c0c62aaf9f7eac01aa639caea
yongchao <xuyongchao58@gmail.com.cn>
2019-06-27 19:36:36 +0800

    八边形

---
+++ src/components/Octagon/index.vue
@@ -0,0 +1,57 @@
+<template>
+  <a class="octagon" :style="{width: `${size}px`, height: `${size}px`}">
+    <span class="wrapper">
+      <span class="inner" :style="{'line-height': `${size}px`, backgroundImage:`url(${url})`}" />
+      <slot />
+    </span>
+  </a>
+</template>
+
+<script>
+export default {
+  props: {
+    size: {
+      type: Number,
+      required: true
+    },
+    url: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+.octagon{
+  display: inline-block;
+  height: 200px;
+  width: 200px;
+  background: yellow;
+  overflow: hidden;
+}
+.wrapper{
+  display: inline-block;
+  width: 100%;
+  height: 100%;
+  /* background-size: cover;
+  background-position: center center;
+  background-color: #000; */
+  /* transform-origin: 50% 50%; */
+  transform: rotate(45deg);
+  text-align: center;
+  line-height: 200px;
+  overflow: hidden;
+  background-color: #000;
+}
+.inner{
+  display: inline-block;
+  width: 100%;
+  height: 100%;
+  transform-origin: -50% -50%;
+  background-size: cover;
+  background-position: center center;
+  background-color:red;
+  transform: rotate(-45deg);
+}
+</style>
--- src/views/guide/index.vue
+++ src/views/guide/index.vue
@@ -8,19 +8,27 @@
     <el-button icon="el-icon-question" type="primary" @click.prevent.stop="guide">
       Show Guide
     </el-button>
+    <Octagon :size="50" :url="url" />
+    <Octagon :size="50" />
+    <Octagon :size="50" />
   </div>
 </template>
 
 <script>
+import Octagon from '@/components/Octagon'
 import Driver from 'driver.js' // import driver.js
 import 'driver.js/dist/driver.min.css' // import driver.js css
 import getSteps from './steps'
 let vm = null
 export default {
   name: 'Guide',
+  components: {
+    Octagon
+  },
   data() {
     return {
       show: false,
+      url: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1561632953997&di=3eab27d54ef209d8d417def66cbc7519&imgtype=0&src=http%3A%2F%2Fimg02.tooopen.com%2Fimages%2F20151231%2Ftooopen_sy_153310261897.jpg',
       driver: null
     }
   },
